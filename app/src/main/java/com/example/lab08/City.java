b50aa7b660bcede59a8e659a6ccd0bd97db604e3
OwaisAmir-1 <27100429@lums.edu.pk>
2026-03-19 15:23:14 +0500

    CI check

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -3,6 +3,7 @@ package com.example.lab08;
 public class City {
     private String city;
     private String province;
+    // CI check
 
     public City(String city, String province){
         this.city = city;
