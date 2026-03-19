20b2414b1e2290145a27e210a0968b0749abe27a
OwaisAmir-1 <27100429@lums.edu.pk>
2026-03-19 15:48:45 +0500

    RED: Add failing test for countCities()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,7 +3,7 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CustomListTest {
 
     @Test
@@ -24,5 +24,15 @@ public class CustomListTest {
         // RED: this should fail until deleteCity is implemented
         list.deleteCity(calgary);
         assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        // RED: this should fail until countCities is implemented
+        assertEquals(1, list.countCities());
     }
 }
